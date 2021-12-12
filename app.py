2e00afd7e5b124eceeb0b07acec8fd3d2d67c93a
Igo da Costa Andrade <igoandrade.314@gmail.com>
2021-12-12 19:23:29 +0000

    Adding HTML components to a Dash app

--- app.py
+++ app.py
@@ -7,7 +7,26 @@ app = dash.Dash(__name__)
 
 # Create the app's layout:
 app.layout = html.Div([
-    html.H1('Hello World!')
+    html.H1(
+        children='Poverty And Equity Database',
+        style={
+            'color': 'blue',
+            'fontSize': '32px',
+            'marginLeft': '20%'
+        }
+    ),
+    html.H2('The World Bank'),
+    html.P('Key Facts:'),
+    html.Ul([
+        html.Li('Number of Economies: 170'),
+        html.Li('Temporal Coverage: 1974 - 2019'),
+        html.Li('Update Frequency: Quarterly'),
+        html.Li('Last Update: March 18, 2020'),
+        html.Li([
+            'Source: ',
+            html.A('https://datacatalog.worldbank.org/dataset/poverty-and-equity-database', href='https://datacatalog.worldbank.org/dataset/poverty-and-equity-database')
+        ])
+    ])
 ])
 
 # Run the app:

fdc4f38a05acd86e57ac074e75380a139a0f5e28
Igo da Costa Andrade <igoandrade.314@gmail.com>
2021-12-18 00:07:12 +0000

    App interativo usando Dash em um Jupyter notebook (Capítulo 02 - primeira parte)

---
+++ chap02/app.py
@@ -0,0 +1,37 @@
+# Uso de notebooks Jupyter para rodar aplicativos Dash
+
+## Inportação dos pacotes requeridos
+from dash import html
+import dash_core_components as dcc
+from jupyter_dash import JupyterDash
+from dash.dependencies import Output, Input
+
+## Instanciação do 'app'
+app = JupyterDash(__name__)
+
+## Layout do 'app'
+app.layout = html.Div([
+    html.H1('OI'),
+    dcc.Dropdown(
+        id='color_dropdown',
+        options=[
+            {'label': color, 'value': color} for color in ['red', 'green', 'yellow']
+        ]
+    ),
+    html.Br(),
+    html.Div(id='color_output')
+])
+
+
+# Especificando a função de callback
+@app.callback(Output(component_id='color_output', component_property='children'),\
+              Input(component_id='color_dropdown', component_property='value'))
+def display_selected_color(color):
+    if color is None:
+        color = 'nothing'
+    return f'You selected {color}.'
+
+## Rodar o 'app'
+## app.run_server(mode='inline', height=600, width='80%')
+if __name__=="__main__":
+    app.run_server(mode='inline')
